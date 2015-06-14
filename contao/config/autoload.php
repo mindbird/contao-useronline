443bbbdf0d5d6c5bb3dcabba0f21c0c5cdada2bd
Wusch <florian.otto@mindbird.de>
2015-06-14 18:31:25 +0200

    Additional changes for composer

---
+++ contao/config/autoload.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by mindbird
+ * User: Florian Otto
+ * Date: 10.04.15
+ * Time: 21:45
+ */
+
+/*
+ * Register the templates
+ */
+TemplateLoader::addFiles(array
+(
+    'be_useronline'       => 'system/modules/useronline/templates',
+));
